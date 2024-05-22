66601af01204991abcedd784708eeb30a3b29ed6
erickgomezmlm <erickgomezmlm@gmail.com>
2024-05-22 08:21:46 -0600

    Second Commit

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Please enter you're grade percent:");
+        int grade = Convert.ToInt32(Console.ReadLine());
+        string grade_claim;
+        if (grade >= 90)
+        {
+            grade_claim = "A";
+        }else if (grade >= 80)
+        {
+            grade_claim = "B";
+        }else if (grade >= 70)
+        {
+            grade_claim = "C";
+        }else if (grade >= 60)
+        {
+            grade_claim = "D";
+        }else {grade_claim = "F";}
+
+        Console.WriteLine("You got a "+grade_claim);
     }
 }
