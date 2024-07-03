335dc37f3c388d646088ca4f28c3b72db2228ae1
erickgomezmlm <erickgomezmlm@gmail.com>
2024-07-02 20:52:39 -0600

    I am currently cathcing up

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Sofware Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2015;
+        job1._endYear = 2018;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Chicfila";
+        job2._startYear = 2010;
+        job2._endYear = 2015;
+
+
+        Resume my_resume = new Resume();
+        my_resume._name = "Erick";
+
+        my_resume._jobs.Add(job1);
+        my_resume._jobs.Add(job2);
+
+        my_resume.Display();
     }
 }
+
